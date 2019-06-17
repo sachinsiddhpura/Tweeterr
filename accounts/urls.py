a3647ae80e455e074ffcc8859ce4fd082ecff458
Sachin Siddhpura <sachinsids007@gmail.com>
2019-06-17 13:59:00 +0530

    tweet

---
+++ accounts/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url, include
+
+from django.views.generic.base import RedirectView
+
+from .views import (
+    UserDetailView,
+    UserFollowView
+    )
+app_name = 'profiles'
+urlpatterns = [
+    url(r'^(?P<username>[\w.@+-]+)/$', UserDetailView.as_view(), name='detail'),
+    url(r'^(?P<username>[\w.@+-]+)/follow/$', UserFollowView.as_view(), name='follow'),]
+
